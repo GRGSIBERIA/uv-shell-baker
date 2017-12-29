c0d6fd3b7c050715723fa61505793f2aa6437b18
Eiichi Takebuchi <nanashi4129@gmail.com>
2017-12-29 10:35:45 +0900

    UV整理用のクラスを実装

---
+++ ArrangeUVIndices.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ArrangeUVIndices
+{
+    static int CountTexture(Material[] materials)
+    {
+        Dictionary<string, int> texturePathes = new Dictionary<string, int>();
+        foreach (var mat in materials)
+        {
+            var tex = AssetDatabase.GetAssetPath(mat.mainTexture);
+            texturePathes[tex] = 1;
+        }
+
+        return texturePathes.Count;
+    }
+
+
+    public static void Arrange(Material[] materials)
+    {
+        int count = CountTexture(materials);
+
+    }
+
+}
