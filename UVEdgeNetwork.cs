5121151fa4e5bebe91bad71ac83cbacdea5052af
Eiichi Takebuchi <nanashi4129@gmail.com>
2017-12-30 18:36:10 +0900

    UVのエッジのネットワークを構成

--- UVEdgeNetwork.cs
+++ UVEdgeNetwork.cs
@@ -1,25 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UVEdgeNetwork
 {
     /// <summary>
-    /// エッジの始点から終点までの
+    /// 始点と終点の頂点番号で構成されるエッジ，重複あり
+    /// p_k \in {p_n, p_n+1, ..., p_n+m}
     /// </summary>
-    public Dictionary<int, List<int>> Network { get; private set; }
+    public readonly List<List<int>> Network;
 
+    public void InitNetwork(int[] triangles, int vertexCount)
+    {
+        List<List<int>> network = new List<List<int>>(vertexCount);
 
+        for (int i = 0; i < vertexCount; ++i)
+        {
+            this.Network.Add(new List<int>());
+        }
+
+        int triangleCount = triangles.Length / 3;
+        for (int i = 0; i < triangleCount; ++i)
+        {
+            int a = i;
+            int b = i + 1;
+            int c = i + 2;
+
+            this.Network[a].Add(b);
+            this.Network[b].Add(c);
+        }
+    }
 
     public UVEdgeNetwork(Mesh mesh)
     {
-        this.Network = new Dictionary<int, List<int>>(mesh.vertexCount);
-
-        int[] triangles = mesh.triangles;
-        int triangleCount = mesh.vertexCount / 3;
-        for (int i = 0; i < triangleCount; ++i)
-        {
-
-        }
+        this.Network = new List<List<int>>();
+        this.InitNetwork(mesh.triangles, mesh.vertexCount);
     }
 }
