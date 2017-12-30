c197f730b2861935f5577b57d84036b5f323aeda
Eiichi Takebuchi <nanashi4129@gmail.com>
2017-12-30 17:25:03 +0900

    UVEdgeを作るためのクラス

---
+++ UVEdgeNetwork.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVEdgeNetwork
+{
+    /// <summary>
+    /// エッジの始点から終点までの
+    /// </summary>
+    public Dictionary<int, List<int>> Network { get; private set; }
+
+
+
+    public UVEdgeNetwork(Mesh mesh)
+    {
+        this.Network = new Dictionary<int, List<int>>(mesh.vertexCount);
+
+        int[] triangles = mesh.triangles;
+        int triangleCount = mesh.vertexCount / 3;
+        for (int i = 0; i < triangleCount; ++i)
+        {
+
+        }
+    }
+}
