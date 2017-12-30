05f0b25fe5785c126be8e92a1d95d7478478c02c
Eiichi Takebuchi <nanashi4129@gmail.com>
2017-12-30 22:07:06 +0900

    ちょっとテストを書く

--- Editor/Test/TestTextureID.cs
+++ Editor/Test/TestTextureID.cs
@@ -11,6 +11,19 @@ public class TestTextureID {
 		// Use the Assert class to test conditions.
 	}
 
+    SkinnedMeshRenderer GetSkinnedMeshRenderer(GameObject utc, string goPath)
+    {
+        return utc.transform.Find(goPath).gameObject.GetComponent<SkinnedMeshRenderer>();
+    }
+
+    [Test]
+    public void TestTexturedMaterial()
+    {
+        var utc = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Models/UnityChan/Prefabs/unitychan.prefab");
+        var mesh = GetSkinnedMeshRenderer(utc, "mesh_root/hair_front");
+        Assert.That(mesh.sharedMaterials.Length, Is.EqualTo(1));
+    }
+
 	// A UnityTest behaves like a coroutine in PlayMode
 	// and allows you to yield null to skip a frame in EditMode
 	[UnityTest]
