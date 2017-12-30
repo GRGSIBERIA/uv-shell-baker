023af7673c28479b5d61e4e6335ca51374dc89b3
Eiichi Takebuchi <nanashi4129@gmail.com>
2017-12-30 22:07:33 +0900

    UVIDから島番号を返す処理を書く

--- UVShellBuilder.cs
+++ UVShellBuilder.cs
@@ -8,20 +8,28 @@ using UnityEngine;
 /// </summary>
 public class UVShellBuilder
 {
+    /// <summary>
+    /// 島番号->UVID
+    /// </summary>
     public readonly List<List<int>> ShellNetwork;
+
+    /// <summary>
+    /// UVID->島番号
+    /// </summary>
+    public readonly int[] AssignedTriangleToShell;
 
     /// <summary>
     /// 未使用のUVのインデックスを探す
     /// </summary>
     /// <param name="usedUVs"></param>
     /// <returns></returns>
-    int SearchNotUsedIndexForNextShell(int[] usedUVs)
+    int SearchNotUsedIndexForNextShell(int[] usedUVs, List<List<int>> shellNetwork)
     {
         for (int i = 0; i < usedUVs.Length; ++i)
         {
             if (usedUVs[i] == 0)
             {
-                ShellNetwork.Add(new List<int>());
+                shellNetwork.Add(new List<int>());
                 return i;
             }
         }
@@ -34,11 +42,11 @@ public class UVShellBuilder
     /// <param name="usedUVs"></param>
     /// <param name="network"></param>
     /// <param name="start"></param>
-    void BreadthFirstSearch(int[] usedUVs, List<List<int>> network, int start)
+    void BreadthFirstSearch(int[] usedUVs, List<List<int>> network, List<List<int>> shellNetwork, int start)
     {
         // UV点を踏んだので使用する
         usedUVs[start] = 1;
-        ShellNetwork.Last().Add(start);
+        shellNetwork.Last().Add(start);
 
         var ends = network[start];
         foreach (var end in ends)
@@ -46,20 +54,40 @@ public class UVShellBuilder
             // 遷移先のUVが使用済みなら無視する
             if (usedUVs[end] == 1)
                 continue;
-            BreadthFirstSearch(usedUVs, network, end);  // 未使用の末端に移動
+            BreadthFirstSearch(usedUVs, network, shellNetwork, end);  // 未使用の末端に移動
         }
+    }
+
+    List<List<int>> BuildShellNetwork(List<List<int>> network, int vertexCount)
+    {
+        int[] usedUVs = Enumerable.Repeat<int>(0, vertexCount).ToArray();
+        var shellNetwork = new List<List<int>>();
+
+        int nextId;
+        while ((nextId = SearchNotUsedIndexForNextShell(usedUVs, shellNetwork)) != -1)    // 次の島を探索する
+        {
+            BreadthFirstSearch(usedUVs, network, shellNetwork, nextId);
+            // 恐らく島の探索は終了したので次の島を探索する
+        }
+        return shellNetwork;
+    }
+
+    int[] BuildAssignedShell(int vertexCount)
+    {
+        int[] uv2shell = new int[vertexCount];
+
+        for (int i = 0; i < this.ShellNetwork.Count; ++i)
+        {
+            for (int j = 0; j < this.ShellNetwork[i].Count; ++j)
+                uv2shell[this.ShellNetwork[i][j]] = i;
+        }
+
+        return uv2shell;
     }
 
     public UVShellBuilder(List<List<int>> network, int vertexCount)
     {
-        int[] usedUVs = Enumerable.Repeat<int>(0, vertexCount).ToArray();
-        this.ShellNetwork = new List<List<int>>();
-
-        int nextId;
-        while ((nextId = SearchNotUsedIndexForNextShell(usedUVs)) != -1)    // 次の島を探索する
-        {
-            BreadthFirstSearch(usedUVs, network, nextId);
-            // 恐らく島の探索は終了したので次の島を探索する
-        }
+        this.ShellNetwork = BuildShellNetwork(network, vertexCount);
+        this.AssignedTriangleToShell = BuildAssignedShell(vertexCount);
     }
 }
