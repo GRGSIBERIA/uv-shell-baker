16cdacee38efa0502740825d471e39d1b6515205
Eiichi Takebuchi <nanashi4129@gmail.com>
2017-12-31 18:52:25 +0900

    ポリゴンの描画処理を追加

--- Editor/BakeTexture.cs
+++ Editor/BakeTexture.cs
@@ -7,21 +7,31 @@ using System.Drawing;
 public class BakeTexture
 {
     public List<Image> Textures { get; private set; }
+    List<System.Drawing.Graphics> graphes;
 
     List<Image> CreateTextures(string[] texturePathes)
     {
         List<Image> textures = new List<Image>();
+        graphes = new List<System.Drawing.Graphics>();      // あとでDisposeしないといけない
         foreach (var path in texturePathes)
         {
             var source = new Bitmap(path);
             var graph = System.Drawing.Graphics.FromImage(source);
             graph.FillRectangle(Brushes.Black, new Rectangle(0, 0, source.Width, source.Height));
-            graph.Dispose();
+            graphes.Add(graph);
             textures.Add(source);
         }
         return textures;
     }
 
+
+
+    /// <summary>
+    /// 島とテクスチャIDがポリゴンに紐付いていないケースをチェックする
+    /// </summary>
+    /// <param name="shell"></param>
+    /// <param name="textureId"></param>
+    /// <param name="id"></param>
     void CheckShellAndTextureOnTriangle(UVShellBuilder shell, TextureID textureId, int[] id)
     {
         for (int i = 0; i < 2; ++i)
@@ -33,6 +43,16 @@ public class BakeTexture
         }
     }
 
+    void DrawTriangle(int[] id, Vector2[] uvpos, Size texSize, System.Drawing.Graphics graph)
+    {
+        Point[] realpos = {
+            new Point((int)(uvpos[id[0]].x * texSize.Width), (int)(uvpos[id[0]].y * texSize.Height)),
+            new Point((int)(uvpos[id[1]].x * texSize.Width), (int)(uvpos[id[1]].y * texSize.Height)),
+            new Point((int)(uvpos[id[2]].x * texSize.Width), (int)(uvpos[id[2]].y * texSize.Height)) };
+
+        graph.FillPolygon(Brushes.Black, realpos);
+    }
+
     void PaintingShell(Vector2[] UVpos, int[] triangles, UVShellBuilder shell, TextureID textureId)
     {
         for (int i = 0; i < triangles.Length; i += 3)
@@ -41,7 +61,8 @@ public class BakeTexture
 
             CheckShellAndTextureOnTriangle(shell, textureId, id);
 
-
+            // id[0]が通用するのは，三角形のすべてで値が一致しているから
+            DrawTriangle(id, UVpos, Textures[id[0]].Size, graphes[id[0]]);
         }
     }
 
