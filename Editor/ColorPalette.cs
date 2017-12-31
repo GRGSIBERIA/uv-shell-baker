281113af42a0bb82ecf09aa579e879dd7130b7f5
Eiichi Takebuchi <nanashi4129@gmail.com>
2017-12-31 19:46:07 +0900

    カラーパレットを実装

--- Editor/ColorPalette.cs
+++ Editor/ColorPalette.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Drawing;
 
-public class ColorPalette : MonoBehaviour {
+/// <summary>
+/// カラーパレット
+/// </summary>
+public class ColorPalette
+{
+    /// <summary>
+    /// 偶数番目の色は鮮やか，奇数番目の色合いはコンストラクタに依存
+    /// </summary>
+    public System.Drawing.Color[] Colors { get; private set; }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /// <summary>
+    /// 島を塗りつぶす色をつくるためのカラーパレット
+    /// </summary>
+    /// <param name="shellCount"></param>
+    /// <param name="sValue">奇数番目の彩度</param>
+    /// <param name="vValue">奇数番目の明度</param>
+    public ColorPalette(int shellCount, float sValue, float vValue)
+    {
+        this.Colors = new System.Drawing.Color[shellCount];
+
+        // shellの数だけ適当な色分けを考える
+        float diff = 1f / (float)Colors.Length;
+        for (int i = 0; i < Colors.Length; ++i)
+        {
+            float s = (i & 1) == 0 ? 1f : sValue;
+            float v = (i & 1) == 0 ? 1f : vValue;
+
+            var c = UnityEngine.Color.HSVToRGB(diff * i, s, v);
+
+            this.Colors[i] = System.Drawing.Color.FromArgb(255, (int)(c.r * 255), (int)(c.g * 255), (int)(c.b * 255));
+        }
+    }
 }
