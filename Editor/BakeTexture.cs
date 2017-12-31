43ac19d5fe2e95fe4b2a33c9748719c6ac0a8529
Eiichi Takebuchi <nanashi4129@gmail.com>
2017-12-31 17:40:37 +0900

    初期状態のテクスチャを生成する

--- Editor/BakeTexture.cs
+++ Editor/BakeTexture.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Drawing;
 
-public class BakeTexture : MonoBehaviour {
+public class BakeTexture
+{
+    List<Image> Textures;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    List<Image> CreateTextures(string[] texturePathes)
+    {
+        List<Image> textures = new List<Image>();
+        foreach (var path in texturePathes)
+        {
+            var img = new Bitmap(path);
+            textures.Add(img);
+        }
+        return textures;
+    }
+
+    public BakeTexture(Vector2[] UVpos, int[] triangles, TextureID textureId, UVShellBuilder shell)
+    {
+        this.Textures = CreateTextures(textureId.TexturePathes);
+    }
 }
