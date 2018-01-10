8f7c997013b143f406cd9f643b52b3ef426158b9
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-01-10 21:09:50 +0900

    UVShellBuilderのテストを追加

---
+++ Editor/Test/TestUVShellBuilder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestUVShellBuilder
+{
+    UVShellBuilder GetUVShellBuilder()
+    {
+        var go = LoadTestModel.Load(LoadTestModel.TestCase.Cube);
+        var render = go.GetComponent<MeshFilter>();
+        var network = new UVEdgeNetwork(render.sharedMesh.triangles);
+        return new UVShellBuilder(network.UVNetwork, render.sharedMesh.vertexCount);
+    }
+
+	[Test]
+	public void TestUVShellBuilderSimplePasses() {
+        // Use the Assert class to test conditions.
+        var builder = GetUVShellBuilder();
+
+        
+    }
+}
