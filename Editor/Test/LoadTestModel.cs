639d224d646cfcb15023265b86e6c30cfaa44854
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-01-10 11:26:59 +0900

    ロード処理

--- Editor/Test/LoadTestModel.cs
+++ Editor/Test/LoadTestModel.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class LoadTestModel
 {
-
+    public static GameObject Load(string path)
+    {
+        return AssetDatabase.LoadAssetAtPath<GameObject>(path);
+    }
 }
