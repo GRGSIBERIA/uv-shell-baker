432a96a3809022fdf65611bb3e9fcefdffcd9d21
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-01-10 11:27:10 +0900

    モデルのロード処理のテスト

---
+++ Editor/Test/TestLoadModel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestLoadModel
+{
+    GameObject Load()
+    {
+        return LoadTestModel.Load("Assets/Scripts/uv-shell-baker/Editor/Test/Models/cube.fbx");
+    }
+
+    [Test]
+    public void LoadModel()
+    {
+        var go = Load();
+        Assert.AreEqual("cube", go.name);
+    }
+}
